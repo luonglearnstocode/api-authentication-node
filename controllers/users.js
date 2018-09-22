17886e8b5b890bcee3ce7c863237c955372aa00c
luonglearnstocode <luong.nguyen@metropolia.fi>
2018-09-22 21:32:02 +0300

    sign json web token

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,32 @@
+const JWT = require('jsonwebtoken')
 const User = require('../models/user')
+const { JWT_SECRET } = require('../config')
+
+const signToken = user => {
+  return JWT.sign({
+    iss: 'authapi',
+    sub: user.id,
+    iat: new Date().getTime(), // current time
+    exp: new Date().setDate(new Date().getDate() + 1) // current time + 1 day
+  }, JWT_SECRET)
+}
 
 module.exports = {
   signUp: async (req, res, next) => {
     const { email, password } = req.value.body
+
     // check if email already exists
     const foundUser = await User.findOne({ email })
     if (foundUser) return res.status(403).json({ error: 'Email already exists' })
+
     // create new user
     const newUser = new User({ email, password })
     await newUser.save()
-    res.status(201).json(newUser)
+
+    // create token
+    const token = signToken(newUser)
+    // respond with token
+    res.status(201).json({ token })
   },
   signIn: async (req, res, next) => {
     console.log('UsersController signin called')
